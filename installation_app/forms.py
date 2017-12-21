8fe86008a3a02eb7bca046a237a0f9b0bd007886
Agri1988 <2690736@gmail.com>
2017-12-21 22:56:34 +0100

    filter_queryset_in_form

--- installation_app/forms.py
+++ installation_app/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from .models import Installation
+from django.contrib.auth.models import User
 
 class InstallationFormUser(forms.ModelForm):
     class Meta:
         model = Installation
         exclude = ['accepted']
+
+    def __init__(self, *args, **kwargs):
+        forms.ModelForm.__init__(self, *args, **kwargs)
+        self.fields['employee_1'].queryset = User.objects.filter(is_staff=False)
+        self.fields['employee_2'].queryset = User.objects.filter(is_staff=False)
+        self.fields['employee_3'].queryset = User.objects.filter(is_staff=False)
 
 class InstallationFormAdmin(forms.ModelForm):
     class Meta:
