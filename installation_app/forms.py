5bad75c7bcc6fbcaf87d4ca081e4279625d25878
Agri1988 <2690736@gmail.com>
2017-12-21 22:58:50 +0100

    filter_queryset_in_form

--- installation_app/forms.py
+++ installation_app/forms.py
@@ -13,7 +13,14 @@ class InstallationFormUser(forms.ModelForm):
         self.fields['employee_2'].queryset = User.objects.filter(is_staff=False)
         self.fields['employee_3'].queryset = User.objects.filter(is_staff=False)
 
+
 class InstallationFormAdmin(forms.ModelForm):
     class Meta:
         model = Installation
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        forms.ModelForm.__init__(self, *args, **kwargs)
+        self.fields['employee_1'].queryset = User.objects.filter(is_staff=False)
+        self.fields['employee_2'].queryset = User.objects.filter(is_staff=False)
+        self.fields['employee_3'].queryset = User.objects.filter(is_staff=False)
